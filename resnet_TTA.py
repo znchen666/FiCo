bcd43a2d5d51393f4f0b8b310dd4435be4c80d02
znchen666 <95161725+znchen666@users.noreply.github.com>
2024-12-14 21:51:33 +0800

    Update resnet_TTA.py

--- resnet_TTA.py
+++ resnet_TTA.py
@@ -1,7 +1,7 @@
 import torch
 from torch import Tensor
 import torch.nn as nn
-from models.dyconv import *
+from dyconv import *
 try:
     from torch.hub import load_state_dict_from_url
 except ImportError:
